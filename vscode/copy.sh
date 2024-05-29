83e41efe1256ecb57be95c07b217a036aacaba0a
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2024-05-29 11:00:33 +0900

    add vscode setting copy script

---
+++ vscode/copy.sh
@@ -0,0 +1,5 @@
+cp /mnt/c/Users/$USER/AppData/Roaming/Code/User/settings.json .
+cp /mnt/c/Users/$USER/AppData/Roaming/Code/User/keybindings.json .
+
+chmod 644 settings.json
+chmod 644 keybindings.json
